9624f767bff9f466e3a07722494302e0996b4f95
Sidney Tan <sidneyjt@uci.edu>
2020-07-16 21:49:54 -0700

    Search results now shows course name(1 table per course).

--- src/search/SearchResults.js
+++ src/search/SearchResults.js
@@ -23,10 +23,21 @@ class SearchResults extends Component{
         Courses.getClasses(payload)
         .then(response =>{
             console.log(response);
+            const classes = response.data.classInfo;
+            let map = {}
+            classes.forEach(item =>{
+                if(map[item.name]){
+                    map[item.name].push(item);
+                }
+                else{
+                    map[item.name] = [];
+                    map[item.name].push(item);
+                }
+            });
             this.setState({
                 result:true,
-                classInfo:response.data.classInfo,
-                classDescription:response.data.classDescription
+                classDescription:response.data.classDescription,
+                classInfo:map
             });
             console.log("Got data!");
         })
@@ -36,72 +47,77 @@ class SearchResults extends Component{
     }
 
     displayClasses = () =>{
-        const {classInfo} = this.state;
+        const{classInfo} = this.state;
         return(
-            classInfo.map((key,index)=>{
-                const {meetingTimes, location} = key;
-                const {meetings} = meetingTimes;
+            Object.keys(classInfo).map((className,num)=>{
                 return(
-                    <Table striped bordered size = "sm" key = {index}>
+                    <div key = {num}>{className}
+                    <Table striped bordered size = "sm" key = {num}>
                         <thead>
-                            <tr>
-                                <th>Add</th>
-                                <th>CRN</th>
-                                <th>Instructor</th>
-                                <th>Times</th>
-                                <th>Location</th>
-                                <th>Enrollment</th>
-                                <th>Status</th>
-                            </tr>
+                                <tr>
+                                    <th>Add</th>
+                                    <th>CRN</th>
+                                    <th>Instructor</th>
+                                    <th>Times</th>
+                                    <th>Location</th>
+                                    <th>Enrollment</th>
+                                    <th>Status</th>
+                                </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td>+</td>
-                                <td>{key.CRN}</td>
-                                <td>{key.instructor}</td>
-                                <td>
-                                    {
-                                        meetings[0].info === "available" ? 
-                                        meetings[0].days + " " + meetings[0].time :
-                                        meetings[0].description
-                                    }
-                                </td>
-                                <td>{location[0]}</td>
-                                <td>{key.activated + "/" + key.capacity}</td>
-                                <td>{key.status}</td>
-                            </tr>
-                            {
-                                meetings.length > 1 &&
-                                meetings.map((key,index) => {
-                                    if(index !== 0){
-                                        return(
-                                            <tr key = {key}>
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
-                                                <td>
-                                                    {
-                                                        key.info === "available" ?
-                                                        key.days + " " + key.time :
-                                                        key.description
+                        {classInfo[className].map((key, index) =>{
+                            const {meetingTimes, location} = key;
+                            const {meetings} = meetingTimes;
+                            return(
+                                <tbody key = {index}>
+                                <tr>
+                                    <td>+</td>
+                                    <td>{key.CRN}</td>
+                                    <td>{key.instructor}</td>
+                                    <td>
+                                        {
+                                            meetings[0].info === "available" ? 
+                                            meetings[0].days + " " + meetings[0].time :
+                                            meetings[0].description
+                                        }
+                                    </td>
+                                    <td>{location[0]}</td>
+                                    <td>{key.activated + "/" + key.capacity}</td>
+                                    <td>{key.status}</td>
+                                </tr>
+                                {
+                                    meetings.length > 1 &&
+                                    meetings.map((key,index) => {
+                                        if(index !== 0){
+                                            return(
+                                                <tr key = {key}>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td>
+                                                        {
+                                                            key.info === "available" ?
+                                                            key.days + " " + key.time :
+                                                            key.description
+                                                        }
+                                                    </td>
+                                                    {location.length > 1 &&
+                                                        <td>{location[index]}</td>
                                                     }
-                                                </td>
-                                                {location.length > 1 &&
-                                                    <td>{location[index]}</td>
-                                                }
-                                                <td></td>
-                                                <td></td>
-                                            </tr>
-                                        );
-                                    }
-                                    else{
-                                        return null;
-                                    }
-                                })
-                            }
-                        </tbody>
-
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                            );
+                                        }
+                                        else{
+                                            return null;
+                                        }
+                                    })
+                                }
+                            </tbody>
+                            );
+                        })}
                     </Table>
+                    </div>
                 );
             })
         );
@@ -115,6 +131,7 @@ class SearchResults extends Component{
                 <div>
                     <this.displayClasses/>
                 </div>
+
             );
         }
         else{
