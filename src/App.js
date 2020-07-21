8d0b9e2d7371285c9561cd9ecfa48d0371b02de4
Sidney Tan <sidneyjt@uci.edu>
2020-07-20 21:10:59 -0700

    Changed App.js to class instead of function.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SearchForm from "./search/SearchForm";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
@@ -6,21 +6,46 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import "./css/column.css"
 
-function App() {
-  return (
-    <div>
-      <Container fluid>
-        <Row>
-          <Col>
-            Schedule, implement soon.
-          </Col>
-          <Col className = "column-scroll">
-            <SearchForm/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedCourses:{}
+    }
+  }
+  
+  pushCourse = (e,course) =>{
+    let {selectedCourses} = this.state;
+    if(!selectedCourses[course.CRN]){
+      const {meetingTimes, location} = course;
+      const {meetings} = meetingTimes;
+      let courseInfo = {
+        meetings:meetings,
+        location:location,
+        name:course.name
+      }
+      selectedCourses[course.CRN] = courseInfo
+      this.setState({
+        selectedCourses: selectedCourses
+      })
+    }
+  }
+  render(){
+    return ( 
+      <div>
+        <Container fluid>
+          <Row>
+            <Col>
+              Schedule, implement soon.
+            </Col>
+            <Col className = "column-scroll">
+              <SearchForm pushCourse = {this.pushCourse}/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
