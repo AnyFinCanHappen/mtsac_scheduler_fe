a7a8ddd3d158cab35e13af110704a2de0d764d7f
Sidney Tan <sidneyjt@uci.edu>
2020-08-12 16:48:58 -0700

    Fixed bug where Block component would crash, instead of calling displayMeetingTimes function, moved function body into displayCards function.

--- src/block/Block.js
+++ src/block/Block.js
@@ -12,53 +12,6 @@ import "../css/search_result.css"
 class Block extends Component{
     state = {}
 
-    displayMeetingTimes = (props) =>{
-        const {meetings, location} = props;
-        return(
-            <Table striped bordered size = "sm" >
-                <thead>
-                    <tr>
-                        <th>Times</th>
-                        <th>Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>
-                    {
-                        meetings[0].info === "available" ? 
-                        meetings[0].days + " " + meetings[0].time :
-                        meetings[0].description
-                    }
-                    </td>
-                    <td>{location[0]}</td>
-                    </tr>
-                    {meetings.length > 1 && meetings.map((key,index)=>{
-                        if(index !== 0){
-                            return(
-                                <tr key = {index}>
-                                    <td>
-                                        {
-                                            key.info === "available" ?
-                                            key.days + " " + key.time :
-                                            key.description
-                                        }
-                                    </td>
-                                    {location.length > 1 &&
-                                        <td>{location[index]}</td>
-                                    }
-                                </tr>
-                            );
-                        }
-                        else{
-                            return null;
-                        }
-                    })} 
-                </tbody>
-            </Table>
-        );
-    }
-
     displayCards = () =>{
         const {selectedCourses} = this.props;
         return(
@@ -67,6 +20,7 @@ class Block extends Component{
                 <Row xs = {1} sm = {2} lg = {3}>
                 {Object.keys(selectedCourses).map((item,index) =>{
                     const course = selectedCourses[item];
+                    console.log(course);
                     const {meetingTimes, location} = course;
                     const {meetings} = meetingTimes;
                     return(
@@ -74,7 +28,7 @@ class Block extends Component{
                         <Card>
                             <Card.Body>
                             <Button size = "sm"  className = "btn-delete" onClick = {(e) => this.props.deleteCourse(e, item) } >
-                                <img src = "../image/trashbin2.png" alt = {"x"} className = "trashbin"></img>
+                                <img src = "../image/trashbin2.png" alt = {"x"} className = "trashbin"/>
                             </Button>
                                 <Card.Title>
                                     {course.name}                                  
@@ -86,7 +40,47 @@ class Block extends Component{
                                     <br></br>
                                     {"Cred:" + course.cred}
                                 </Card.Text>
-                                <this.displayMeetingTimes meetings = {meetings} location = {location}/>
+                                <Table striped bordered size = "sm" >
+                                    <thead>
+                                        <tr>
+                                            <th>Times</th>
+                                            <th>Location</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <td>
+                                        {
+                                            meetings[0].info === "available" ? 
+                                            meetings[0].days + " " + meetings[0].time :
+                                            meetings[0].description
+                                        }
+                                        </td>
+                                        <td>{location[0]}</td>
+                                        </tr>
+                                        {meetings.length > 1 && meetings.map((key,index)=>{
+                                            if(index !== 0){
+                                                return(
+                                                    <tr key = {index}>
+                                                        <td>
+                                                            {
+                                                                key.info === "available" ?
+                                                                key.days + " " + key.time :
+                                                                key.description
+                                                            }
+                                                        </td>
+                                                        {location.length > 1 &&
+                                                            <td>{location[index]}</td>
+                                                        }
+                                                    </tr>
+                                                );
+                                            }
+                                            else{
+                                                return null;
+                                            }
+                                        })} 
+                                    </tbody>
+                                </Table>
                             </Card.Body>
                         </Card>
                         </Col>
