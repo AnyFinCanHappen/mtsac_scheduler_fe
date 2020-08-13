3059a83ba55e06044d85000fabfd0da35cb79687
Sidney Tan <sidneyjt@uci.edu>
2020-08-12 17:22:08 -0700

    added variable for local host testing.

--- src/util/Courses.js
+++ src/util/Courses.js
@@ -1,7 +1,8 @@
 import Socket from "./socket"
 import Constants from "../constants/BackendEP.json"
+
+let local = "http://localhost:3001/api/";   //used for development
 async function getCourses(payload){
-    //let local = "http://localhost:3001/api/";
     let response = await Socket.sendPOSTHTTP(Constants.backendURL, Constants.retrieveClassesEP,payload);
     return response;
 }
